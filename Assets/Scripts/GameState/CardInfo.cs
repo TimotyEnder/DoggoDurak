2f1ad8b75a3ba755b5bb0f67e897c5fe8b34a8a8
TimotyEnder <timotyender@gmail.com>
2025-04-07 22:37:20 +0200

    added start of loading GameState infrastructure

---
+++ Assets/Scripts/GameState/CardInfo.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class CardInfo
+{
+    private string _suit;
+    private int number;
+
+    public CardInfo(string suit, int number) 
+    {
+        this._suit = suit;
+        this.number = number;
+    }
+    public string getSuit() 
+    {
+        return _suit;
+    }
+    public int getNumber() 
+    {
+        return number;
+    }
+}
---
+++ Assets/Scripts/GameState/GameState.cs
@@ -0,0 +1,12 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameState
+{
+    private List<CardInfo> _deck;
+    private int gold;
+
+    
+
+}
