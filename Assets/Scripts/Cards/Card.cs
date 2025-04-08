401479c062328751f65fcdd952434e0518a22af4
TimotyEnder <61980517+TimotyEnder@users.noreply.github.com>
2025-04-08 17:40:54 +0200

    restructured card code now find a way to find the first card that is hovered and now all of them

--- Assets/Scripts/Cards/Card.cs
+++ Assets/Scripts/Cards/Card.cs
@@ -1,13 +1,20 @@
+using System.Runtime.CompilerServices;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 using static TMPro.SpriteAssetUtilities.TexturePacker_JsonArray;
 
-public class Card : MonoBehaviour
+public class Card : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private RectTransform _thisRect;
+    private GameObject _cardHolder;
+    private CardHandArea _cardHandArea;
     void Start()
     {
-        
+        _thisRect = this.GetComponent<RectTransform>();
+        _cardHolder = GameObject.Find("CardHandArea");
+        _cardHandArea = _cardHolder.GetComponent<CardHandArea>();
     }
 
     // Update is called once per frame
@@ -20,13 +27,22 @@ public class Card : MonoBehaviour
     }
     public void OnDraw() 
     {
-        RectTransform thisRect = this.GetComponent<RectTransform>();
-        GameObject CardHolder = GameObject.Find("CardHandArea");
-        CardHandArea cardHandArea = CardHolder.GetComponent<CardHandArea>();
-        this.GetComponent<RectTransform>().SetParent(CardHolder.GetComponent<RectTransform>());
-        thisRect.anchoredPosition = cardHandArea.AttachCard();
+        _cardHolder = GameObject.Find("CardHandArea");
+        _cardHandArea = _cardHolder.GetComponent<CardHandArea>();
+        this.GetComponent<RectTransform>().SetParent(_cardHolder.GetComponent<RectTransform>());
+        _thisRect.anchoredPosition = _cardHandArea.AttachCard();
         //cardHandArea.RestackHand();
         this.GetComponent<RectTransform>().SetSiblingIndex(0);
+    }
+    public void OnPointerEnter(PointerEventData eventData) 
+    {
+        //this._thisRect.SetSiblingIndex(0);
+       // this._thisRect.localScale = new Vector3(1.5f, 1.5f, 1.5f);
+    }
+    public void OnPointerExit(PointerEventData eventData)
+    {
+       // this._cardHandArea.RestackHand();
+       // this._thisRect.localScale = new Vector3(1f, 1f, 1f);
     }
 }
 
