f9234d4df31b97f51b1e5d4b732d4b5c84daac3f
TimotyEnder <timotyender@gmail.com>
2025-06-25 23:34:34 +0200

    Add BabushkasBorsh item and implementation
    Introduced a new item, BabushkasBorsh, including its asset, meta files, and C# implementation. The item applies the 'Restoring' modifier to three random cards in the deck upon acquisition.

---
+++ Assets/Scripts/Item/ItemTypes/BabushkasBorsh.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+[CreateAssetMenu(fileName = "BabushkasBorsh", menuName = "Items/Common/BabushkasBorsh")]
+public class BabushkasBorsh : Item
+{
+    public override void InitItem()
+    {
+        this.rarity = 0;
+        this.boss = false;
+        this.ItemId = "BabushkasBorsh";
+    }
+
+    public override void OnAquire()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            GameHandler.Instance.GetGameState()._deck[Random.Range(0, GameHandler.Instance.GetGameState()._deck.Count - 1)].addModifier("Restoring");
+        }
+    }
+
+    public override void OnDefendCard(Card defendee, Card defended)
+    {
+    }
+
+    public override void OnLoad()
+    {
+    }
+
+    public override void OnPlayedCard(Card card)
+    {
+    }
+
+    public override void OnReverse(Card card)
+    {
+    }
+}
