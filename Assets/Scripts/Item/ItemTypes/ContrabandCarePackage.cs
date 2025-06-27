6c5332697c0e6f54930929353ccdaf6a7bb6d8a3
TimotyEnder <timotyender@gmail.com>
2025-06-27 13:59:09 +0200

    Add ContrabandCarePackage item and script
    Introduces the ContrabandCarePackage item as a ScriptableObject with its associated asset and script. The item, when acquired, adds 'Draw' modifiers to two cards in the player's deck.

---
+++ Assets/Scripts/Item/ItemTypes/ContrabandCarePackage.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+[CreateAssetMenu(fileName = "ContrabandCarePackage", menuName = "Items/Common/ContrabandCarePackage")]
+public class ContrabandCarePackage : Item
+{
+    public override void InitItem()
+    {
+        this.rarity = 0;
+        this.boss = false;
+        this.ItemId = "ContrabandCarePackage";
+    }
+
+    public override void OnAquire()
+    {
+        int cardsModded = 0;
+        int it = 0;
+        int amountToMod = 2;
+        string modifier = "Draw";
+        while (it < GameHandler.Instance.GetGameState()._deck.Count && cardsModded < amountToMod)
+        {
+            CardInfo cardToMod = GameHandler.Instance.GetGameState()._deck[Random.Range(0, GameHandler.Instance.GetGameState()._deck.Count - 1)];
+            if (!cardToMod._modifierStacks.ContainsKey(modifier))
+            {
+                cardToMod.AddModifier(modifier);
+                cardsModded++;
+            }
+            it++;
+        }
+        for (int j = 0; j < amountToMod - cardsModded; j++) //try top add modifiers even if one instance of them is on every card. Sigleton modifiers handled internally by addModifier()
+        {
+            CardInfo cardToMod = GameHandler.Instance.GetGameState()._deck[Random.Range(0, GameHandler.Instance.GetGameState()._deck.Count - 1)];
+            cardToMod.AddModifier(modifier);
+        }
+    }
+
+    public override void OnDefendCard(Card defendee, Card defended)
+    {
+    }
+
+    public override void OnLoad()
+    {
+    }
+
+    public override void OnPlayedCard(Card card)
+    {
+    }
+
+    public override void OnReverse(Card card)
+    {
+    }
+}
