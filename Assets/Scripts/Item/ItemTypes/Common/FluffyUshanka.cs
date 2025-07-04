38effac0362bb1da6bdde2caef8f072ddcefd1fc
TimotyEnder <61980517+TimotyEnder@users.noreply.github.com>
2025-07-04 20:00:46 +0200

    implemented fluffy ushanka and made a common items folder

---
+++ Assets/Scripts/Item/ItemTypes/Common/FluffyUshanka.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+[CreateAssetMenu(fileName = "FluffyUshanka", menuName = "Items/Common/FluffyUshanka")]
+public class FluffyUshanka : Item
+{
+    public override void InitItem()
+    {
+        this.rarity = 0;
+        this.boss = false;
+        this.ItemId = "FluffyUshanka";
+    }
+
+    public override void OnAquire()
+    {
+
+    }
+
+    public override void OnDefendCard(Card defendee, Card defended)
+    {
+
+    }
+
+    public override void OnHeal(int amount)
+    {
+        GameHandler.Instance.HealPlayer(5);
+    }
+
+    public override void OnLoad()
+    {
+
+    }
+
+    public override void OnPlayedCard(Card card)
+    {
+
+    }
+
+    public override void OnReverse(Card card)
+    {
+
+    }
+}
