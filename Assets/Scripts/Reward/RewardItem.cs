95c1a0da9644a50c2bd8f1f23dfa0f9ba31c5997
TimotyEnder <timotyender@gmail.com>
2025-09-13 17:43:56 +0200

    reward items can now have a price

--- Assets/Scripts/Reward/RewardItem.cs
+++ Assets/Scripts/Reward/RewardItem.cs
@@ -9,6 +9,7 @@ public class RewardItem : MonoBehaviour
     private Image _bgColor;
     private Button _button;
     private RewardItemGrid _grid;
+    private int price = 0;
 
     private void Start()
     {
@@ -17,8 +18,12 @@ public class RewardItem : MonoBehaviour
         _grid= GameObject.Find("RewardItemGrid").GetComponent<RewardItemGrid>(); 
     }
 
-    public void AssignItem(Item item)
+    public void AssignItem(Item item, int itemPrice=0)
     {
+        if (itemPrice > 0) 
+        {
+            this.price = itemPrice;
+        }
         _toolTip = GetComponent<ToolTip>();
         this._item = item;
         this._itemIcon = item.GetIcon();
@@ -42,9 +47,13 @@ public class RewardItem : MonoBehaviour
     }
     private void OnClickActiveItem()
     {
-        GameHandler.Instance.GetGameState().AddItem(this._item);
-        GetComponent<ToolTip>().SetTooltipActiveState(false);
-        _grid.ChoiceHappened();
-        Destroy(this.gameObject);
+        if (price==0 || price>0 && GameHandler.Instance.GetGameState()._rubles>price) 
+        {
+            GameHandler.Instance.GetGameState().AddItem(this._item);
+            GetComponent<ToolTip>().SetTooltipActiveState(false);
+            _grid.ChoiceHappened();
+            if(price>0) { GameHandler.Instance.GetGameState()._rubles-=price; } 
+            Destroy(this.gameObject);
+        }
     }
 }
