3a40b7b7753dcbf88ed8b70ed763a136e0d75cbd
TimotyEnder <timotyender@gmail.com>
2025-10-30 11:14:58 +0100

    added Bouncer Dvorniashka

---
+++ Assets/Scripts/Encounter/EncounterTypes/Boss/BouncerDvorniashka.cs
@@ -0,0 +1,75 @@
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "BouncerDvorniashka", menuName = "Encounters/Boss/BouncerDvorniashka")]
+public class BouncerDvorniashka : Encounter
+{
+    public override void InitEncounter()
+    {
+        deck = new List<CardInfo>(); //standart durak deck initialization
+        for (int i = 0; i < 4; i++)
+        {
+            switch (i)
+            {
+                case 0:
+                    for (int j = 6; j < 15; j++)
+                    {
+                        deck.Add(new CardInfo("C", j, true));
+                    }
+                    break;
+                case 1:
+                    for (int j = 6; j < 15; j++)
+                    {
+                        deck.Add(new CardInfo("S", j, true));
+                    }
+                    break;
+                case 2:
+                    for (int j = 6; j < 15; j++)
+                    {
+                        deck.Add(new CardInfo("D", j, true));
+                    }
+                    break;
+                case 3:
+                    for (int j = 6; j < 15; j++)
+                    {
+                        deck.Add(new CardInfo("H", j, true));
+                    }
+                    break;
+            }
+        }
+        foreach (CardInfo c in deck) 
+        {
+            if (c._number%2==0)
+            {
+                c.AddModifier("Bounce");
+                c.AddModifier("Burn");
+                c.AddModifier("Burn");
+            }
+        }
+        goldRewardMod = 2.0f;
+        health = 100;
+        trumpSuit = 'R';
+        icon = null;
+        boss = true;
+        day = 0;
+    }
+
+    public override void OnDamagePlayer(int amount)
+    {
+
+    }
+
+    public override void OnDefendCard(Card card, Card defendedWith)
+    {
+
+    }
+
+    public override void OnPlayedCard(Card card)
+    {
+
+    }
+
+    public override void OnReverse(Card card)
+    {
+
+    }
+}
