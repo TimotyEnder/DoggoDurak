d10a0fb0e71ed92cf9643890c464fdf46300f13a
TimotyEnder <timotyender@gmail.com>
2025-11-05 10:43:54 +0100

    small delay handler comment

--- Assets/Scripts/GameHandlers/DelayHandler.cs
+++ Assets/Scripts/GameHandlers/DelayHandler.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class DelayHandler
 {
-    public static float DelayTimeModifierEffect = 0.1f;
+    public static float DelayTimeModifierEffect = 0.1f; //later on just take the global settings number
     //yep, this method exists just becuase i cant wait for a float amount of time with Task.Delay(), sigh...
     public static async Task DelayFloat(float seconds)
     {
