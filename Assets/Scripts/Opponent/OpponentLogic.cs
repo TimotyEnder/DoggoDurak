c20cfefdb4b421a6f25e538e029d0d651a36cb64
TimotyEnder <timotyender@gmail.com>
2026-02-18 22:50:43 +0100

    refactored turn system to work with a flag array for both turn States not every type of interaction works as intended

--- Assets/Scripts/Opponent/OpponentLogic.cs
+++ Assets/Scripts/Opponent/OpponentLogic.cs
@@ -25,7 +25,7 @@ public class OpponentLogic : MonoBehaviour
     private bool endTurnCaused = false;//flag to stop end turn infinite loop 
     private bool _enemyPlaying = false;
     private CardHandArea _cardHandArea;
-    private bool _doublePass=false; //if you pass once the enemy gets to put more cards down and then if you press pass again you will not defend.
+    private bool[] _doublePass={false,false}; //if you pass once the enemy gets to put more cards down and then if you press pass again you will not defend.
     private Discard _discard;
     private void Start()
     {
@@ -208,21 +208,21 @@ public class OpponentLogic : MonoBehaviour
         }
         Debug.Log("Enemy Turn Routine!");
         CardHandArea cha = GameObject.Find("CardHandArea").GetComponent<CardHandArea>();
-        if (!endTurnCaused && cha != null && (!cha.HasMorePlays() || _doublePass && _turnHandler.GetTurnState()!=0)) 
+        if (!endTurnCaused && cha != null && (!cha.HasMorePlays() || _doublePass[_turnHandler.GetTurnState()])) 
         {
             endTurnCaused = true;
             _turnHandler.StartEndTurn();
         }
         else
         {
-            _doublePass=true;
+            _doublePass[_turnHandler.GetTurnState()]=true;
         }
         _enemyPlaying = false;
         _cardHandArea.GreyOutAllCards();
     }
     public void resetDoublePass()
     {
-        _doublePass=false;
+        _doublePass=new bool[]{false,false};
     }
     public void resetEndTurnFlag() 
     {
