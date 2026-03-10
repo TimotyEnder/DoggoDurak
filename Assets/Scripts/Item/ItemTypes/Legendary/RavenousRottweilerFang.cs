f74320e7e4885128aaffb40537b8caac95f3a51d
TimotyEnder <timotyender@gmail.com>
2026-03-10 11:44:24 +0100

    ravenous rotweiler fang implemented

---
+++ Assets/Scripts/Item/ItemTypes/Legendary/RavenousRottweilerFang.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+[CreateAssetMenu(fileName = "RavenousRottweilerFang", menuName = "Items/Legendary/RavenousRottweilerFang")]
+class RavenousRottweilerFang : Item
+{
+    public override void InitItem()
+    {
+        this.rarity = 2;
+        this.boss = false;
+        this.itemId = "RavenousRottweilerFang";
+        this.toolTipDesc = $"When you deal >{StylisticClass.DamageNumber(1)} at once, heal {StylisticClass.HighLight} 1hp{StylisticClass.HighLightClose}";
+    }
+
+    public override void OnActivate()
+    {
+        
+    }
+
+    public override void OnAquire()
+    {
+        
+    }
+
+    public override void OnDamageOpponent(int amount, string fromMod = "")
+    {
+        if(amount>1)
+        {
+            GameHandler.Instance.HealPlayer(1);
+        }
+    }
+
+    public override void OnDefendCard(Card defendee, Card defended)
+    {
+        
+    }
+
+    public override void OnEndEncounter()
+    {
+        
+    }
+
+    public override void OnHeal(int amount)
+    {
+        
+    }
+
+    public override void OnLoad()
+    {
+        
+    }
+
+    public override void OnPlayedCard(Card card)
+    {
+        
+    }
+
+    public override void OnReverse(Card card)
+    {
+        
+    }
+
+    public override void OnTurnEnd(int turnState)
+    {
+        
+    }
+}
